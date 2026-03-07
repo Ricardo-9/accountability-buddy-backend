ce5f9c5b082b5ca2ab32e9b677a5d04341815c30
kaiky <kaikyro11@gmail.com>
2026-03-07 14:18:19 -0300

    feat: implement middleware with Zod to validate requests

---
+++ src/middlewares/validateRequest.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express"
+import { z } from "zod"
+
+export function validateRequest(schema: z.ZodType) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const result = schema.safeParse({
+            body: req.body,
+            params: req.params,
+            query: req.query
+        })
+
+        if (!result.success) {
+            return next(result.error)
+        }
+
+        const data = result.data as {
+            body: Request["body"],
+            params: Request["params"],
+            query: Request["query"]
+        }
+
+        req.body = data.body
+        req.params = data.params
+        req.query = data.query
+
+        next()
+    }
+}
