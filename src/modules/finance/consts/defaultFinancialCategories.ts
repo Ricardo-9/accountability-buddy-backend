284f45d799b37837f7cca776787a9b5be5865da5
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-03-22 15:45:57 -0300

    feat: add default categories const

---
+++ src/modules/finance/consts/defaultFinancialCategories.ts
@@ -0,0 +1,11 @@
+export const DEFAULT_FINANCIAL_CATEGORIES = [
+  "HOUSING",
+  "FOOD",
+  "TRANSPORTATION",
+  "UTILITIES",
+  "HEALTH",
+  "EDUCATION",
+  "DEBTS",
+  "SAVINGS",
+  "OTHER",
+] as const;
