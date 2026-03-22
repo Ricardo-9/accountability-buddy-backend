d6b14661fde5fecb07cbb465e6450118255b7a63
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-03-22 16:09:21 -0300

    feat: add financialCategories controllers

---
+++ src/modules/finance/controllers/financialCategories.controller.ts
@@ -0,0 +1,70 @@
+import { Request, Response, NextFunction } from "express";
+import { successResponse } from "../../../shared/utils/apiResponse.js";
+import { financialCategoriesServices } from "../services/financialCategories.service.js";
+import { updateFinancialCategoryBody, updatefinancialCategoryId } from "../schemas/updateCategory.schema.js";
+import { deletefinancialCategoryId } from "../schemas/deleteCategory.schema.js";
+export const financialCategoriesControllers = {
+  async getCategories(req: Request, res: Response, next: NextFunction) {
+    const userId = req.user!.id;
+    try {
+      const categories =
+        await financialCategoriesServices.getCategories(userId);
+
+      return successResponse(res, categories);
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  async updateCategory(
+    req: Request<updatefinancialCategoryId, any, updateFinancialCategoryBody>,
+    res: Response,
+    next: NextFunction,
+  ) {
+    const userId = req.user!.id;
+    const { name } = req.body;
+    const { id } = req.params;
+    try {
+      const updatedCategory = await financialCategoriesServices.updateCategory(
+        userId,
+        id,
+        name,
+      );
+
+      return successResponse(res, updatedCategory, "Category updated");
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  async createCategory(req: Request, res: Response, next: NextFunction) {
+    const userId = req.user!.id;
+    const { name } = req.body;
+    try {
+      const category = await financialCategoriesServices.createCategory(
+        userId,
+        name,
+      );
+
+      return successResponse(res, category, "Category created");
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  async deleteCategory(
+    req: Request<deletefinancialCategoryId, any>,
+    res: Response,
+    next: NextFunction,
+  ) {
+    const userId = req.user!.id;
+    const { id } = req.params;
+
+    try {
+      await financialCategoriesServices.deleteCategory(userId, id);
+      return successResponse(res, null, "Category deleted");
+    } catch (err) {
+      next(err);
+    }
+  },
+};
