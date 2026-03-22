be6351e5d77488b749dc5f3ed5a5ffe86f7101d7
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-03-22 16:07:14 -0300

    feat: create validation schemas for createCategory,deleteCategory and updateCategory

---
+++ src/modules/finance/schemas/updateCategory.schema.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const updateFinancialCategorySchema = z.object({
+  body: z.object({
+    name: z
+      .string({
+        error: (iss) => {
+          if (iss.code === "invalid_type")
+            return "name for category must be a string";
+          if (iss.input === undefined) return "name for category is required";
+          return "Invalid name for category";
+        },
+      })
+      .min(2, { error: "The minimum number of characters is 2" })
+      .max(120, { error: "The maximum number of characters is 120" }),
+  }),
+  params: z.object({
+    id: z.string().uuid(),
+  })
+});
+
+export type updateFinancialCategoryBody = z.infer<typeof updateFinancialCategorySchema>["body"]
+export type updatefinancialCategoryId = z.infer<typeof updateFinancialCategorySchema>["params"]
