0a3ba25349cc79bb863be7c125839331a3ddc1ac
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-03-26 10:35:28 -0300

    feat(variableExpenses): add get (one specific) variable expense controller

--- src/modules/finance/controllers/variableExpense.controller.ts
+++ src/modules/finance/controllers/variableExpense.controller.ts
@@ -3,6 +3,22 @@ import { variableExpenseService } from "../services/variableExpenses.service.js"
 import { successResponse } from "../../../shared/utils/apiResponse.js";
 
 export const variableExpenseController = {
+  async getVariableExpense(req: Request, res: Response, next: NextFunction) {
+    const userId = req.user!.id;
+    const expenseId = req.body;
+
+    try {
+      const variableExpense = await variableExpenseService.getVariableExpense(
+        userId,
+        expenseId,
+      );
+
+      return successResponse(res, variableExpense);
+    } catch (err) {
+      next(err);
+    }
+  },
+
   async getVariableExpenes(req: Request, res: Response, next: NextFunction) {
     const userId = req.user!.id;
 
