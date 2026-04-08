12c81f07c2e8f2e0b9d874165c9fade67cbbabc2
kaiky <kaikyro11@gmail.com>
2026-04-08 13:18:36 -0300

    test(recurringTransaction): add unit test to createRecurringTransaction service

--- src/modules/finance/controllers/createrecurringtransaction.controller.ts
+++ src/modules/finance/controllers/createrecurringtransaction.controller.ts
@@ -4,12 +4,12 @@ import { createRecurringTransactionService } from "../services/createrecurringtr
 import { successResponse } from "../../../shared/utils/apiResponse.js";
 
 export async function createRecurringTransactionController(
-    req: Request,
+    req: Request<CreateRecurringTransaction>,
     res: Response,
     next: NextFunction
 ) {
     const userId = req.user!.id
-    const data = req.body as CreateRecurringTransaction
+    const data = req.body
 
     try {
         const transaction = await createRecurringTransactionService(userId, data)
