029f0dabb21018feaea48274cd4be6c3642f0ed6
Ricardo-9 <ricardorochaalves550@gmail.com>
2026-04-23 11:26:46 -0300

    tests(variableExpenses): add integration tests for create variable expense route

--- tests/integration/routes/finance/financial-variable-expenses/createVariableExpense.test.ts
+++ tests/integration/routes/finance/financial-variable-expenses/createVariableExpense.test.ts
@@ -4,6 +4,7 @@ import app from "../../../../../src/app.js";
 import { AppError } from "../../../../../src/core/errors/AppError.js";
 import { authenticate } from "../../../../../src/middlewares/authMiddleware.js";
 import { Prisma } from "@prisma/client";
+import { createVariableExpenseService } from "../../../../../src/modules/finance/variable-expenses/services/createVariableExpense.service.js";
 
 const mockExpenseDb = {
   id: "3fd12663-f4df-4fcf-a67a-83e3035338ca",
@@ -21,12 +22,11 @@ const mockExpenseResponse = {
   updatedAt: mockExpenseDb.updatedAt.toISOString(),
 };
 
-
 let requireAreaShouldFail = false;
 let requireFinancialAccountShouldFail = false;
 
 vi.mock(
-  "../../../../../src/modules/finance/financial-categories/services/getOneCategory.service.js",
+  "../../../../../src/modules/finance/variable-expenses/services/createVariableExpense.service.js",
 );
 
 vi.mock("../../../../../src/middlewares/authMiddleware.js", () => ({
@@ -68,4 +68,139 @@ beforeEach(() => {
 
   requireAreaShouldFail = false;
   requireFinancialAccountShouldFail = false;
+});
+
+describe("create variable expense integration test", () => {
+  it("should create and return the variabe expense for the user", async () => {
+    vi.mocked(createVariableExpenseService).mockResolvedValue(mockExpenseDb);
+
+    const response = await request(app).post("/finance/variable-expense").send({
+      name: "Random Expense",
+      amount: 234.45,
+      expenseDate: "2025-04-01",
+    });
+    expect(response.body.data.variableExpense).toEqual(mockExpenseResponse);
+    expect(response.status).toBe(200);
+  });
+  it("should create expense without category", async () => {
+    vi.mocked(createVariableExpenseService).mockResolvedValue(mockExpenseDb);
+
+    const response = await request(app).post("/finance/variable-expense").send({
+      name: "Random Expense",
+      amount: 234.45,
+      expenseDate: "2025-04-01",
+    });
+
+    expect(response.status).toBe(200);
+  });
+  it("should return 400 when name does not exist", async () => {
+    const response = await request(app).post("/finance/variable-expense").send({
+      amount: 234.45,
+      expenseDate: "2025-04-01",
+    });
+
+    expect(response.status).toBe(400);
+  });
+  it("should return 400 when name is too short", async () => {
+    const response = await request(app)
+      .post("/finance/variable-expense")
+      .send({ name: "A", amount: 234.45, expenseDate: "2025-04-01" });
+
+    expect(response.status).toBe(400);
+  });
+  it("should return 400 when name is too long", async () => {
+    const response = await request(app).post("/finance/variable-expense").send({
+      name: "DDLpTqRvWxYzAbCdEfGhIjKlMnOpQrStUvWxYzAbCdEfGhIjKlMnOpQrStUvWxYzAbCdEfGhIjKlMnOpQrStUvWxYzAbCdEfGhIjKlMnOpQrStUvWxYzAbCdE",
+      amount: 234.45,
+      expenseDate: "2025-04-01",
+    });
+
+    expect(response.status).toBe(400);
+  });
+  it("should return 400 when categoryId is invalid", async () => {
+    const response = await request(app).post("/finance/variable-expense").send({
+      name: "Random Expense",
+      amount: 234.45,
+      expenseDate: "2025-04-01",
+      categoryId: "invalid-id",
+    });
+
+    expect(response.status).toBe(400);
+  });
+
+  it("should return 400 when amount is invalid", async () => {
+    const response = await request(app).post("/finance/variable-expense").send({
+      name: "Random Expense",
+      amount: "invalid",
+      expenseDate: "2025-04-01",
+    });
+
+    expect(response.status).toBe(400);
+  });
+
+  it("should return 401 when user is not authenticated", async () => {
+    vi.mocked(authenticate).mockImplementationOnce(async (_req, _res, next) => {
+      next(new AppError("UNAUTHORIZED", "Invalid or expired token", 401));
+    });
+
+    const response = await request(app).post("/finance/variable-expense").send({
+      name: "Random Expense",
+      amount: 234.45,
+      expenseDate: "2025-04-01",
+    });
+
+    expect(response.status).toBe(401);
+  });
+
+  it("should return 403 when user does not have area permission", async () => {
+    requireAreaShouldFail = true;
+
+    const response = await request(app).post("/finance/variable-expense").send({
+      name: "Random Expense",
+      amount: 234.45,
+      expenseDate: "2025-04-01",
+    });
+
+    expect(response.status).toBe(403);
+  });
+
+  it("should return 404 when category does not exist", async () => {
+    vi.mocked(createVariableExpenseService).mockRejectedValue(
+      new AppError("INVALID_REFERENCE", "Category not found", 404),
+    );
+
+    const response = await request(app).post("/finance/variable-expense").send({
+      name: "Random Expense",
+      amount: 234.45,
+      expenseDate: "2025-04-01",
+      categoryId: "3fd12663-f4df-4fcf-a67a-83e3035338ca",
+    });
+
+    expect(response.status).toBe(404);
+  });
+
+  it("should throw error 404 (NOT_FOUND) if user does not have an account", async () => {
+    requireFinancialAccountShouldFail = true;
+    const response = await request(app).post("/finance/variable-expense").send({
+      name: "Random Expense",
+      amount: 234.45,
+      expenseDate: "2025-04-01",
+    });
+    expect(response.status).toBe(404);
+    expect(response.body.error.message).toBe("User account not found");
+  });
+
+  it("should return 500 when database fails", async () => {
+    vi.mocked(createVariableExpenseService).mockRejectedValue(
+      new Error("DB error"),
+    );
+
+    const response = await request(app).post("/finance/variable-expense").send({
+      name: "Random Expense",
+      amount: 234.45,
+      expenseDate: "2025-04-01",
+    });
+
+    expect(response.status).toBe(500);
+  });
 });
