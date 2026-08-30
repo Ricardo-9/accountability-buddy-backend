b9157fe2878a63213549c879e1f4b840777894a9
Ricardo Rocha Alves <166510367+Ricardo-9@users.noreply.github.com>
2026-08-30 20:16:07 -0300

    Comment out requireFinancialAccount middleware
    Comment out the requireFinancialAccount middleware code.

--- src/modules/finance/middlewares/requireFinancialAccount.ts
+++ src/modules/finance/middlewares/requireFinancialAccount.ts
@@ -1,25 +1,25 @@
-import { Request, Response, NextFunction } from "express";
-import { prisma } from "../../../lib/prisma.js";
-import { AppError } from "../../../core/errors/AppError.js";
+// import { Request, Response, NextFunction } from "express";
+// import { prisma } from "../../../lib/prisma.js";
+// import { AppError } from "../../../core/errors/AppError.js";
 
-export async function requireFinancialAccount(
-  req: Request,
-  _res: Response,
-  next: NextFunction,
-) {
-  try {
-    const userId = req.user!.id;
+// export async function requireFinancialAccount(
+//   req: Request,
+//   _res: Response,
+//   next: NextFunction,
+// ) {
+//   try {
+//     const userId = req.user!.id;
 
-    const account = await prisma.financeAccount.findUnique({
-      where: { userId },
-      select: { id: true },
-    });
+//     const account = await prisma.financeAccount.findUnique({
+//       where: { userId },
+//       select: { id: true },
+//     });
 
-    if (!account)
-      throw new AppError("NOT_FOUND", "User account not found", 404);
+//     if (!account)
+//       throw new AppError("NOT_FOUND", "User account not found", 404);
 
-    next();
-  } catch (err) {
-    next(err);
-  }
-}
+//     next();
+//   } catch (err) {
+//     next(err);
+//   }
+// }
