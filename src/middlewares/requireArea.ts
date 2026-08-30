b731fc47e01b5e7ede46c4f3e533511a37d4b677
Ricardo Rocha Alves <166510367+Ricardo-9@users.noreply.github.com>
2026-08-30 20:15:14 -0300

    Comment out requireArea middleware implementation

--- src/middlewares/requireArea.ts
+++ src/middlewares/requireArea.ts
@@ -1,28 +1,28 @@
-import { AccountabilityArea } from "@prisma/client";
-import { Request, Response, NextFunction } from "express";
-import { AppError } from "../core/errors/AppError.js";
-import { prisma } from "../lib/prisma.js";
+// // import { AccountabilityArea } from "@prisma/client";
+// import { Request, Response, NextFunction } from "express";
+// import { AppError } from "../core/errors/AppError.js";
+// import { prisma } from "../lib/prisma.js";
 
-export function requireArea(area: AccountabilityArea) {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const userId = req.user!.id;
+// export function requireArea(area: AccountabilityArea) {
+//   return async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const userId = req.user!.id;
 
-      const hasArea = await prisma.userArea.findFirst({
-        where: { userId, area },
-        select: { userId: true },
-      });
+//       const hasArea = await prisma.userArea.findFirst({
+//         where: { userId, area },
+//         select: { userId: true },
+//       });
 
-      if (!hasArea)
-        throw new AppError(
-          "FORBIDDEN",
-          `User need to be registered in ${area} to access this feature`,
-          403,
-        );
+//       if (!hasArea)
+//         throw new AppError(
+//           "FORBIDDEN",
+//           `User need to be registered in ${area} to access this feature`,
+//           403,
+//         );
 
-      next();
-    } catch (err) {
-      next(err);
-    }
-  };
-}
+//       next();
+//     } catch (err) {
+//       next(err);
+//     }
+//   };
+// } 
